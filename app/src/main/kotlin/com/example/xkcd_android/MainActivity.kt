f00d62e608735cd2c723636509161b66c2d41b40
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-10 21:27:45 +0300

    1

--- app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
+++ app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val CURRENT_COMIC_LINK = "https://xkcd.com/info.0.json"
+    private val COMIC_LINK_BY_NUMBER = "https://xkcd.com/%d/info.0.json"
+
+    private fun get_comic_link_by_number(number: Int): String {
+        return String.format(COMIC_LINK_BY_NUMBER, number)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
