3670260481acc42feed5b8e005fb30612b0dffe6
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-11 17:54:16 +0300

    2

--- app/src/main/kotlin/com/example/xkcd_android/Retrofit.kt
+++ app/src/main/kotlin/com/example/xkcd_android/Retrofit.kt
@@ -3,31 +3,37 @@
 package com.example.xkcd_android
 
 import retrofit2.Call
-import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+data class Comic(
+    val month: String,
+    val num: Int,
+    val link: String,
+    val year: String,
+    val news: String,
+    val safeTitle: String,
+    val transcript: String,
+    val alt: String,
+    val img: String,
+    val title: String,
+    val day: String
+)
 
-object Retrofit {
-    interface Client {
-        @GET("https://xkcd.com/info.0.json")
-        fun getComic(): Callback<Comic>
+interface Interface {
+    @GET("info.0.json")
+    fun getComic(): Call<Comic>
 
-        @GET("https://xkcd.com/{number}/info.0.json")
-        fun getComic(@Path("number") number: Int): Callback<Comic>
-    }
-
-    data class Comic(
-        val month: String,
-        val num: Int,
-        val link: String,
-        val year: String,
-        val news: String,
-        val safeTitle: String,
-        val transcript: String,
-        val alt: String,
-        val img: String,
-        val title: String,
-        val day: String
-    )
+    @GET("{number}/info.0.json")
+    fun getComic(@Path("number") number: Int): Call<Comic>
 }
+
+val retrofit: Retrofit = Retrofit.Builder()
+    .baseUrl("https://xkcd.com/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val instance: Interface = retrofit.create(Interface::class.java)
+
