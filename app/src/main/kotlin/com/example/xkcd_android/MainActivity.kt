261aed114a9485ddf3f3a579e01c0cf98496066a
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-12 18:17:30 +0300

    2

--- app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
+++ app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
@@ -10,16 +10,20 @@ import com.example.core.Comic
 import com.example.core.Repository
 import com.example.retrofit.RepositoryImpl
 import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     private var buttonCurrent: Button? = null
     private var buttonRandom: Button? = null
+    private var buttonSelect: Button? = null
+    private var buttonFirst: Button? = null
+    private var buttonLatest: Button? = null
 
     private var textView: TextView? = null
 
     private val callback = object : Callback<Comic?> {
         override fun onSuccess(value: Comic?, message: String) {
-            textView?.text = message
+            textView?.text = value.toString() + message
         }
 
         override fun onFailure(t: Throwable) {
@@ -28,13 +32,27 @@ class MainActivity : AppCompatActivity() {
     }
     private val repository: Repository = RepositoryImpl(callback)
 
+    private val comicNumberFirst = 1
+    private var comicNumberLatest = 1
+
+    private var comicNumberRange = comicNumberFirst..comicNumberLatest
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         buttonCurrent = findViewById(R.id.current)
         buttonRandom = findViewById(R.id.random)
+        buttonSelect = findViewById(R.id.select)
+        buttonFirst = findViewById(R.id.first)
+        buttonLatest = findViewById(R.id.latest)
+
         textView = findViewById(R.id.text_view)
+
         buttonCurrent?.setOnClickListener { repository.requestComic() }
-        buttonRandom?.setOnClickListener { repository.requestComic(Random.nextInt()) }
+        buttonRandom?.setOnClickListener { repository.requestComic(comicNumberRange.random()) }
+        buttonSelect?.setOnClickListener { repository.requestComic(5) }
+        buttonFirst?.setOnClickListener { repository.requestComic(comicNumberFirst) }
+        buttonLatest?.setOnClickListener { repository.requestComic(comicNumberLatest) }
     }
 }
