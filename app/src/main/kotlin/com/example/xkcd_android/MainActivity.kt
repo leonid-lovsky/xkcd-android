5e50eaf947599e014d1fbf2b782dcef9b93e66ca
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-20 16:24:04 +0300

    a

--- app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
+++ app/src/main/kotlin/com/example/xkcd_android/MainActivity.kt
@@ -96,8 +96,6 @@ class MainActivity : AppCompatActivity(), Presenter, View.OnClickListener {
         val comic = uiState.comic ?: return
         Picasso.get()
             .load(comic.img)
-            // .override(Target.SIZE_ORIGINAL)
-            // .dontTransform()
             .into(comicImageView!!)
         title = comic.title
         comicTitleView!!.text = comic.title
