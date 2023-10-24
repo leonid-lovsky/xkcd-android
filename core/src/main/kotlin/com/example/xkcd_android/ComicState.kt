71c95546e7336358dd68b60d0cf95f830a63aa1c
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-24 11:49:01 +0300

    Commit

--- core/src/main/kotlin/com/example/xkcd_android/ComicState.kt
+++ core/src/main/kotlin/com/example/xkcd_android/ComicState.kt
@@ -1,18 +1,20 @@
 package com.example.xkcd_android
 
 data class ComicState(
-    val comic: Comic? = null,
-    val current: Int = 1,
-    val last: Int = 1,
+    val comic: Comic,
+    val current: Int,
+    val last: Int,
 ) {
     val first = ComicState.first
-    val range get() = first..last
+    val next = current + 1
+    val previous = current - 1
+    val range = first..last
 
     fun copy(comic: Comic): ComicState {
         return copy(comic = comic, current = comic.num, last = maxOf(last, comic.num))
     }
 
-    private companion object {
-        const val first = 1
+    companion object {
+        private const val first = 1
     }
 }
