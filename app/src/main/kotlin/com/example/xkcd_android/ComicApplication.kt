bc598ec8698d3106c8ff896f36e0f1603184833d
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-27 18:12:47 +0300

    Commit

--- app/src/main/kotlin/com/example/xkcd_android/ComicApplication.kt
+++ app/src/main/kotlin/com/example/xkcd_android/ComicApplication.kt
@@ -4,11 +4,12 @@ import android.app.Application
 import java.util.concurrent.Executors
 
 class ComicApplication : Application(), ComicDependencies {
+    private val threadPoolSize = Runtime.getRuntime().availableProcessors()
     private val comicDependenciesLocal = ComicDependenciesRoom(this)
     private val comicDependenciesRemote = ComicDependenciesRetrofit()
     private val comicStorageLocal = comicDependenciesLocal.comicStorageLocal()
     private val comicStorageRemote = comicDependenciesRemote.comicStorageRemote()
-    private val executorServiceWorker = Executors.newFixedThreadPool(3)
+    private val executorServiceWorker = Executors.newFixedThreadPool(threadPoolSize)
     private val comicInteractor = ComicInteractor(comicStorageLocal, comicStorageRemote, executorServiceWorker)
     private val comicController = ComicController(comicInteractor)
 
