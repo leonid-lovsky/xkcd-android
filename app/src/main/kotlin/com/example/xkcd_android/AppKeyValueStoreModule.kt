bf778200d59ec36345436d3b28a2c73dec83127b
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-10-31 14:24:32 +0200

    Commit

--- app/src/main/kotlin/com/example/xkcd_android/AppKeyValueStoreModule.kt
+++ app/src/main/kotlin/com/example/xkcd_android/AppKeyValueStoreModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import com.example.xkcd_android.module.KeyValueStoreModule
 
 class AppKeyValueStoreModule(applicationContext: Context) : KeyValueStoreModule {
+    // private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
     private val sharedPreferences = applicationContext.getSharedPreferences(
         "comic-preferences", MODE_PRIVATE
     )
