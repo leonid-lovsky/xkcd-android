317c38d67c003841c4fb9e66792365faed207661
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-11-03 15:58:06 +0200

    Commit

--- core/src/main/kotlin/com/example/xkcd_android/CorePresenter.kt
+++ core/src/main/kotlin/com/example/xkcd_android/CorePresenter.kt
@@ -28,7 +28,6 @@ class CorePresenter(
     override fun restoreState() {
         repository.loadCurrentNumber(currentNumberCallback)
         repository.loadLatestNumber(latestNumberCallback)
-        repository.loadCurrentComic()
     }
 
     override fun saveState() {
@@ -78,102 +77,15 @@ class CorePresenter(
         TODO("Not yet implemented")
     }
 
-    inner class CurrentNumberCallback : Callback<Int> {
-        override fun callback(value: Int) {
+    inner class CurrentNumberCallback : Callback<Int?> {
+        override fun callback(value: Int?) {
             this@CorePresenter.current = value
         }
     }
 
-    inner class LatestNumberCallback : Callback<Int> {
-        override fun callback(value: Int) {
+    inner class LatestNumberCallback : Callback<Int?> {
+        override fun callback(value: Int?) {
             this@CorePresenter.latest = value
         }
     }
-    // override fun invoke(value: Resource<Comic>) {
-    //     if (value.loading) {
-    //         view?.showProgress()
-    //     } else {
-    //         view?.hideProgress()
-    //     }
-    //     val comic = value.data
-    //     if (comic != null) {
-    //         comicState = comicState.copy(comic)
-    //         view?.render(comicState)
-    //     }
-    //     val error = value.error
-    //     if (error != null) {
-    //         view?.render(error)
-    //     }
-    // }
-    //
-    // fun create() {
-    //     val comicState = comicPreferences.restoreInstanceState()
-    //     if (comicState == null) {
-    //         repository.comic(this)
-    //     }
-    //     if (this.comicState.comic == null) {
-    //         repository.comic(this.comicState.current, this)
-    //     }
-    //     if (comicState?.last == comicState?.first) {
-    //         repository.comic(this)
-    //     }
-    // }
-    //
-    // fun start(comicView: ComicView) {
-    //     this.view = comicView
-    //     this.view?.render(comicState)
-    // }
-    //
-    // fun start() {
-    //     view = null
-    // }
-    //
-    // fun stop() {
-    //     // TODO: thread
-    //     comicPreferences.saveInstanceState(comicState)
-    // }
-    //
-    // fun restoreState() {
-    //     // TODO: thread
-    //     val comicState = comicPreferences.restoreInstanceState()
-    //     if (comicState != null) {
-    //         this.comicState = comicState
-    //     }
-    // }
-    //
-    // fun latest() {
-    //     repository.comic(this)
-    // }
-    //
-    // fun select() {
-    //     view?.showSelectComicDialog()
-    // }
-    //
-    // fun select(number: Int) {
-    //     repository.comic(number, this)
-    // }
-    //
-    // fun first() {
-    //     repository.comic(comicState.first, this)
-    // }
-    //
-    // fun last() {
-    //     repository.comic(comicState.last, this)
-    // }
-    //
-    // fun previous() {
-    //     repository.comic(comicState.current - 1, this)
-    // }
-    //
-    // fun next() {
-    //     repository.comic(comicState.current + 1, this)
-    // }
-    //
-    // fun refresh() {
-    //     repository.comic(comicState.current, this)
-    // }
-    //
-    // fun random() {
-    //     repository.comic(Random.nextInt(comicState.first, comicState.last), this)
-    // }
 }
