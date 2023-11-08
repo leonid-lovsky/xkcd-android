6dd66624210443661d8d09051ccca56cf1d33e62
Leonid Lovsky <leonid.lovsky@yandex.ru>
2023-11-08 17:37:57 +0200

    debug

--- domain/src/main/kotlin/com/example/xkcd_android/ComicViewController.kt
+++ domain/src/main/kotlin/com/example/xkcd_android/ComicViewController.kt
@@ -1,24 +1,158 @@
 package com.example.xkcd_android
 
-interface ComicViewController {
+import kotlin.math.max
+import kotlin.random.Random
 
-    fun setView(view: ComicView?) // TODO: finite state machine
+class ComicViewController(
+    private val repository: ComicDataInteractor
+) : ComicViewController {
 
-    fun restoreState() // TODO: finite state machine
-    fun saveState() // TODO: finite state machine
+    // TODO: finite state machine, shared view state, use-case interactor
+    private var view: ComicView? = null
+    private var comicData: ComicData? = null
+    private var currentNumber: Int? = null
+    private var latestNumber: Int? = null
 
-    fun loadLatestComic() // TODO: use-case interactor
+    private val currentNumberCallback = CurrentNumberCallback()
+    private val latestNumberCallback = LatestNumberCallback()
 
-    fun loadFirstComic() // TODO: finite state machine
-    fun loadLastComic() // TODO: finite state machine
-    fun loadPreviousComic() // TODO: finite state machine
-    fun loadNextComic() // TODO: finite state machine
+    private val comicResourseCallback = ComicResourseCallback()
 
-    fun refreshCurrentComic() // TODO: finite state machine
+    override fun setView(view: ComicView?) {
+        // TODO: finite state machine
+        this.view = view
+        val comic = this.comicData
+        if (comic != null) {
+            view?.render(comic)
+        }
+    }
 
-    fun displaySelectComicDialog() // TODO: ?
+    override fun restoreState() {
+        // TODO: finite state machine
+        repository.loadCurrentNumber(currentNumberCallback)
+        repository.loadLatestNumber(latestNumberCallback)
+    }
 
-    fun loadComicByNumber(number: Int) // TODO: use-case interactor
+    override fun saveState() {
+        // TODO: finite state machine
+        val current = this.currentNumber
+        if (current != null) {
+            repository.saveCurrentNumber(current)
+        }
+        val latest = this.latestNumber
+        if (latest != null) {
+            repository.saveLatestNumber(latest)
+        }
+    }
 
-    fun loadRandomComic() // // TODO: finite state machine
+    override fun loadLatestComic() {
+        // TODO: finite state machine
+        repository.loadLatestComic(comicResourseCallback)
+    }
+
+    override fun loadFirstComic() {
+        // TODO: application constants
+        repository.loadComicByNumber(1, comicResourseCallback)
+    }
+
+    override fun loadLastComic() {
+        // TODO: finite state machine
+        val latestNumber = this.latestNumber
+        if (latestNumber != null) {
+            repository.loadComicByNumber(latestNumber, comicResourseCallback)
+        }
+    }
+
+    override fun loadPreviousComic() {
+        // TODO: finite state machine
+        val currentNumber = this.currentNumber
+        if (currentNumber != null) {
+            repository.loadComicByNumber(currentNumber - 1, comicResourseCallback)
+        }
+    }
+
+    override fun loadNextComic() {
+        // TODO: finite state machine
+        val currentNumber = this.currentNumber
+        if (currentNumber != null) {
+            repository.loadComicByNumber(currentNumber + 1, comicResourseCallback)
+        }
+    }
+
+    override fun refreshCurrentComic() {
+        // TODO: finite state machine
+        val currentNumber = this.currentNumber
+        if (currentNumber != null) {
+            repository.loadComicByNumber(currentNumber, comicResourseCallback)
+        }
+    }
+
+    override fun displaySelectComicDialog() {
+        // TODO: view responsobility
+        view?.displaySelectComicDialog()
+    }
+
+    override fun loadComicByNumber(number: Int) {
+        // TODO: use-case interactor
+        repository.loadComicByNumber(number, comicResourseCallback)
+    }
+
+    override fun loadRandomComic() {
+        // TODO: finite state machine
+        val latestNumber = this.latestNumber
+        if (latestNumber != null) {
+            repository.loadComicByNumber(Random.nextInt(1, latestNumber), comicResourseCallback)
+        }
+    }
+
+    inner class CurrentNumberCallback : BaseCallback<Int?> {
+
+        override fun invoke(value: Int?) {
+            if (value != null) {
+                repository.loadComicByNumber(value, comicResourseCallback)
+            } else {
+                repository.loadLatestComic(comicResourseCallback)
+            }
+        }
+    }
+
+    inner class LatestNumberCallback : BaseCallback<Int?> {
+
+        // TODO: finite state machine
+        override fun invoke(value: Int?) {
+            this@ComicViewController.latestNumber = value
+        }
+    }
+
+    inner class ComicResourseCallback : BaseCallback<BaseResource<ComicData>> {
+
+        // TODO: check if memory leak exists
+        override fun invoke(value: BaseResource<ComicData>) {
+            if (value.loading) {
+                view?.showProgress()
+            } else {
+                view?.hideProgress()
+            }
+
+            if (value.data != null) {
+                currentNumber = value.data.num // TODO: finite state machine
+                val latestNumber = this@ComicViewController.latestNumber // TODO: architecture
+                if (latestNumber != null) {
+                    this@ComicViewController.latestNumber = max(latestNumber, value.data.num)
+                } else {
+                    this@ComicViewController.latestNumber = value.data.num  // TODO: finite state machine
+                }
+
+                view?.render(value.data)
+            }
+
+            if (value.message != null) {
+                view?.render(value.message)
+            }
+
+            if (value.error != null) {
+                view?.render(value.error)
+            }
+        }
+    }
 }
