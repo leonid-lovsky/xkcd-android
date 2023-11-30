f7ceaf325842af7e93d38c1ab812716e068e297f
Leonid Lovsky <leonid.lovsky@gmail.com>
2023-11-30 16:08:07 +0200

    Rewrite

--- retrofit/src/main/kotlin/com/example/xkcd_android/RetrofitComicStorage.kt
+++ retrofit/src/main/kotlin/com/example/xkcd_android/RetrofitComicStorage.kt
@@ -2,15 +2,15 @@ package com.example.xkcd_android
 
 class RetrofitComicStorage(private val retrofitComicService: RetrofitComicService) :
     RemoteComicStorage {
-    override fun getComicByNumber(number: Int): Comic? {
-        val call = retrofitComicService.getRetrofitComicByNumber(number)
+    override fun getLatestComic(): Comic? {
+        val call = retrofitComicService.getLatestRetrofitComic()
         val response = call.execute()
         val body = response.body()
         return body?.toComic()
     }
 
-    override fun getLatestComic(): Comic? {
-        val call = retrofitComicService.getLatestRetrofitComic()
+    override fun getComicByNumber(number: Int): Comic? {
+        val call = retrofitComicService.getRetrofitComicByNumber(number)
         val response = call.execute()
         val body = response.body()
         return body?.toComic()
