b86ba65aaf75a820b29b39b485265c3bf9838d75
Leonid Lovsky <leonid.lovsky@gmail.com>
2023-12-21 19:15:11 +0200

    Port/Adapter

--- app/src/main/kotlin/com/example/xkcd_android/ComicViewModel.kt
+++ app/src/main/kotlin/com/example/xkcd_android/ComicViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -20,20 +22,25 @@ class ComicViewModel @Inject constructor(
 
     operator fun invoke(action: ComicAction) {
         Timber.i(action.toString())
-        _state.value = reduce(_state.value!!, action)
-    }
-
-    private fun reduce(state: ComicState, action: ComicAction): ComicState {
-        Timber.i(state.toString(), action.toString())
-        return when (action) {
-            is ComicAction.GetComic -> state
-            ComicAction.GetFirstComic -> state
-            ComicAction.GetLastComic -> state
-            ComicAction.GetLatestComic -> state
-            ComicAction.GetNextComic -> state
-            ComicAction.GetPreviousComic -> state
-            ComicAction.GetRandomComic -> state
-            ComicAction.RefreshComic -> state
+        viewModelScope.launch {
+            try {
+                _state.value = _state.value?.copy(loading = true)
+                val comic = when (action) {
+                    is ComicAction.GetComic -> _state.value?.comic
+                    ComicAction.GetFirstComic -> _state.value?.comic
+                    ComicAction.GetLastComic -> _state.value?.comic
+                    ComicAction.GetLatestComic -> _state.value?.comic
+                    ComicAction.GetNextComic -> _state.value?.comic
+                    ComicAction.GetPreviousComic -> _state.value?.comic
+                    ComicAction.GetRandomComic -> _state.value?.comic
+                    ComicAction.RefreshComic -> _state.value?.comic
+                }
+                _state.value = _state.value?.copy(comic = comic)
+            } catch (t: Throwable) {
+                Timber.e(t)
+            } finally {
+                _state.value = _state.value?.copy(loading = false)
+            }
         }
     }
 }
