216862a3e67cf770046e1213dde38157f6cf437f
Leonid Lovsky <leonid.lovsky@gmail.com>
2023-12-30 20:25:35 +0200

    MVVM

--- app/src/main/kotlin/com/example/xkcd_android/ComicViewModel.kt
+++ app/src/main/kotlin/com/example/xkcd_android/ComicViewModel.kt
@@ -59,67 +59,17 @@ class ComicViewModel @Inject constructor(
 
     fun getComic(number: Int) {
         Timber.i("${this::class.simpleName}")
-        viewModelScope.launch {
-            try {
-                _loading.value = true
-            } catch (e: Throwable) {
-                Timber.e(e)
-            } finally {
-                _loading.value = false
-            }
-        }
+        _currentComicNumber.value = number
     }
 
     fun refreshComic(number: Int) {
         Timber.i("${this::class.simpleName}")
-        viewModelScope.launch {
-            try {
-                _loading.value = true
-                val response = comicService.getComic(number)
-                Timber.d("response = ${response}")
-                val body = response.body()
-                Timber.d("body = ${body}")
-                if (body != null) {
-                    comicDao.putComic(body)
-                }
-                val comic = comicDao.getComic(number)
-                Timber.d("comic = ${comic}")
-                _comic.value = comic
-            } catch (e: Throwable) {
-                Timber.e(e)
-            } finally {
-                _loading.value = false
-            }
-        }
-    }
-
-    fun refreshComic() {
-        Timber.i("${this::class.simpleName}")
-        val currentComicNumber = _currentComicNumber.value
-        if (currentComicNumber != null) {
-            _currentComicNumber.value = 0
-        }
-    }
-
-    fun getRandomComic() {
-        Timber.i("${this::class.simpleName}")
-        val latestComicNumber = _latestComicNumber.value
-        if (latestComicNumber != null) {
-            _currentComicNumber.value = Random.nextInt(1, latestComicNumber)
-        }
+        _currentComicNumber.value = number
     }
 
     fun getFirstComic() {
         Timber.i("${this::class.simpleName}")
         _currentComicNumber.value = 1
-    }
-
-    fun getLastComic() {
-        Timber.i("${this::class.simpleName}")
-        val latestComicNumber = _latestComicNumber.value
-        if (latestComicNumber != null) {
-            _currentComicNumber.value = latestComicNumber
-        }
     }
 
     fun getPreviousComic() {
@@ -130,11 +80,35 @@ class ComicViewModel @Inject constructor(
         }
     }
 
+    fun refreshComic() {
+        Timber.i("${this::class.simpleName}")
+        val currentComicNumber = _currentComicNumber.value
+        if (currentComicNumber != null) {
+            refreshComic(currentComicNumber)
+        }
+    }
+
     fun getNextComic() {
         Timber.i("${this::class.simpleName}")
         val currentComicNumber = _currentComicNumber.value
         if (currentComicNumber != null) {
             _currentComicNumber.value = currentComicNumber + 1
+        }
+    }
+
+    fun getLastComic() {
+        Timber.i("${this::class.simpleName}")
+        val latestComicNumber = _latestComicNumber.value
+        if (latestComicNumber != null) {
+            _currentComicNumber.value = latestComicNumber
+        }
+    }
+
+    fun getRandomComic() {
+        Timber.i("${this::class.simpleName}")
+        val latestComicNumber = _latestComicNumber.value
+        if (latestComicNumber != null) {
+            _currentComicNumber.value = Random.nextInt(1, latestComicNumber)
         }
     }
 }
